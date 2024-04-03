8a9129feb6c78abc9688a37ee912e71e94e9874f
ThalesDFerreira <thalesferreira190@gmail.com>
2024-04-03 17:37:49 -0300

    feat: configura app, main e add rotas home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import ListDemands from './pages/CadastroClientes';
+import AddDemand from './pages/VisitarClientes';
+import { Toaster } from 'react-hot-toast';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Toaster position='top-right' reverseOrder={false} />
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route exact path='/' element={<Navigate to='/home' />} />
+        <Route path='/lista-demandas' element={<ListDemands />} />
+        <Route path='/add-demanda' element={<AddDemand />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+
+
+  return (
+    <p>
+      Funcionou!!!
+    </p>
+  );
+};
+
+export default Home;
