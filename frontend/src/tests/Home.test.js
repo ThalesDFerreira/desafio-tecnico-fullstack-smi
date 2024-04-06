93bfcc4e8c323cf9725e86dfe76b7f1bfa3f1dbc
ThalesDFerreira <thalesferreira190@gmail.com>
2024-04-06 12:21:26 -0300

    fix: atualizando requests postman e add mais testes home frontend

--- frontend/src/tests/Home.test.js
+++ frontend/src/tests/Home.test.js
@@ -1,13 +1,11 @@
 import '@testing-library/jest-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
 import { screen } from '@testing-library/react';
 import customRender from './utils/index';
-import Home from '../pages/Home';
+import { act } from 'react-dom/test-utils';
 import App from '../App';
 
 describe('Testando página Home', async () => {
-  it('Verifica se existe três botões na página"', () => {
+  it('Verifica se existe três botões na página', () => {
     customRender(<App />, '/home');
 
     const btnElHeader = screen.getByRole('button', {
@@ -30,6 +28,16 @@ describe('Testando página Home', async () => {
     expect(btnElListDemands).toBeInTheDocument();
   });
 
+  it('Verifica se uma mensagem de bem-vindo na página', () => {
+    customRender(<App />, '/home');
+
+    const titleEl = screen.getByRole('heading', {
+      name: /bem\-vindo ao sistema de planejamento de demandas/i,
+    });
+
+    expect(titleEl).toBeInTheDocument();
+  });
+
   it('Verifica se ao clicar no botão Cadastrar Demanda é redirecionado para a página AddDemand', async () => {
     const { user, history } = customRender(<App />, '/home');
 
@@ -37,14 +45,23 @@ describe('Testando página Home', async () => {
       name: /cadastrar demandas/i,
     });
 
-    await user.click(btnElAddDemand);
-
-    history.push('/add-demanda');
+    await act(async () => {
+      await user.click(btnElAddDemand);
+      history.push('/add-demanda');
+    });
 
     const titleElPageAddDemand = screen.getByText('Adicionar nova demanda:');
 
     expect(titleElPageAddDemand).toBeInTheDocument();
     expect(history.location.pathname).toBe('/add-demanda');
+  });
+
+  it('Verifica se existe um footer com o nome da empresa Latinhas LLC', async () => {
+    customRender(<App />, '/home');
+
+    const nameEl = screen.getByText('Latinhas LLC');
+
+    expect(nameEl).toBeInTheDocument();
   });
 
   it('Verifica se ao clicar no botão Lista Demandas é redirecionado para a página ListDemands', async () => {
@@ -54,14 +71,14 @@ describe('Testando página Home', async () => {
       name: /listar demandas/i,
     });
 
-    await user.click(btnElListDemands);
-
-    history.push('/lista-demandas');
+    await act(async () => {
+      await user.click(btnElListDemands);
+      history.push('/add-demanda');
+    });
 
     const titleElPageListDemands = screen.getByText('Lista de Demandas:');
 
     expect(titleElPageListDemands).toBeInTheDocument();
     expect(history.location.pathname).toBe('/lista-demandas');
   });
-
 });
